ba2929993a6a4760d498f030396089175329720f
Aleksanr Bobrov <al.bobrov.mail@gmail.com>
2017-06-26 22:29:40 +0800

    Refactor for jobs log

--- app/interactors/cangaroo/perform_jobs.rb
+++ app/interactors/cangaroo/perform_jobs.rb
@@ -11,14 +11,18 @@ module Cangaroo
     private
 
     def enqueue_jobs(type, payload)
+      enqueued_jobs = []
+      skipped_jobs = []
       initialize_jobs(type, payload).each do |job|
         if job.perform?
-          Cangaroo.logger.info 'Enqueue job:', enqueued_job: job.class.to_s
+          enqueued_jobs << job.class.to_s
           job.enqueue
         else
-          Cangaroo.logger.info 'Skip job:', skipped_job: job.class.to_s
+          skipped_jobs << job.class.to_s
         end
       end
+      Cangaroo.logger.info 'Enqueu jobs:', enqueued_jobs: enqueued_jobs,
+                                           skipped_jobs: skipped_jobs
     end
 
     def initialize_jobs(type, payload)
